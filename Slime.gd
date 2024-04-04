7e9fa981e9b1437b4d4309c77b23a3c06a891c92
Nick <darkforceyoda@gmail.com>
2024-04-04 09:16:46 -0500

    mob removed on health == 0

--- Slime.gd
+++ Slime.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+var health = 3
+
 #var player
 #func _ready():
 	#player = get_node("/root/MainScene/Player")
@@ -10,3 +12,8 @@ func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+
+func take_damage():
+	health -=1
+	if health == 0:
+		queue_free()
