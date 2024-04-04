4c082eebd193e7524be464fa1a448d23621b406e
Nick <darkforceyoda@gmail.com>
2024-04-04 09:10:55 -0500

    bullet spawn on timer

--- pistol/gun.gd
+++ pistol/gun.gd
@@ -5,3 +5,14 @@ func _physics_process(delta):
 	if enemies_in_range.size() > 0: 
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+func _shoot():
+	const BULLET = preload("res://pistol/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %BulletSpawn.global_position
+	new_bullet.global_rotation = %BulletSpawn.global_rotation
+	%BulletSpawn.add_child(new_bullet)
+
+
+func _on_timer_timeout():
+	_shoot()
