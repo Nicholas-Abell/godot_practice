b8f3ed8f4c0f5f737e234138e12a3974ba2584af
Nick <darkforceyoda@gmail.com>
2024-04-04 08:38:25 -0500

    weapon movement/mob tracking

--- Slime.gd
+++ Slime.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-var player
-
-func _ready():
-	player = get_node("/root/MainScene/Player")
+#var player
+#func _ready():
+	#player = get_node("/root/MainScene/Player")
+	
+@onready var player = get_node("/root/MainScene/Player")
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
